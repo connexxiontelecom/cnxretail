c0360284606d0e4c2e83de6f46dd3816f64d2727
Joseph <talktojoegee@gmail.com>
2021-03-25 14:35:39 +0100

    Bulk SMS

--- app/Http/Controllers/Backend/BBNSMSCallsController.php
+++ app/Http/Controllers/Backend/BBNSMSCallsController.php
@@ -26,8 +26,13 @@ class BBNSMSCallsController extends Controller
         }
     }
     public function sendMessage(){
+
         $mobile = "+2348032404359";
-        $name = "Joseph";
+        $termiiApi = "TLpMHRqPFlfmSJPxYONy6qCSs94qkaP3oocjtREGoUq7bAneOm6UEo01mzNdJm";
+        $data = array("to" => "2347880234567","from" => "retail",
+        "sms"=>"Hi there, testing Termii","type" => "plain","channel" => "generic","api_key" => $termiiApi);
+
+      /*   $name = "Joseph";
 
         $message = "CNX Retail Bulk SMS test ";
         $createdURL = "";
@@ -39,9 +44,34 @@ class BBNSMSCallsController extends Controller
         $ozMessageData = $message;
 
         $createdURL = $ozSURL."?username=".trim($ozUser)."&password=".trim($ozPassw)."&sender=".trim($ozMessageType)."&mobile=".trim($ozRecipient)."&message=".$ozMessageData;
+ */
+        /* $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', $createdURL); */
+        /* $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', $data); */
+        $curl = curl_init();
+        $data = array("to" => "2348032404359","from" => "Retail",
+        "sms"=>"Hi there, testing Termii","type" => "plain","channel" => "generic","api_key" => $termiiApi);
+        $post_data = json_encode($data);
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://termii.com/api/sms/send",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $post_data,
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json"
+        ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        echo $response;
 
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', $createdURL);
+
         #Register log
         $log = new ActivityLog;
         $log->subject = "Send Bulk SMS";
